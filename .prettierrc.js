f56e03f651ccba26cbdf95e5f168da24013c16e3
Md Irshad <mdirshadengineer+github@gmail.com>
2025-02-12 19:34:54 +0300

    chore:  📄added prettier global configuration

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  endOfLine: 'lf',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  arrowParens: 'avoid'
+};
+
+module.exports = prettierConfig;
